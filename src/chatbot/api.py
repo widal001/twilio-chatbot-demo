3d09f47d45fd0515a64d208123897f48fbc524ea
widal001 <williamdaly422@gmail.com>
2021-12-14 21:21:37 -0500

    feat(api): Adds POST /surveys and PUT /surveys/{survey_id} endpoints

--- src/chatbot/api.py
+++ src/chatbot/api.py
@@ -1,4 +1,9 @@
+from __future__ import annotations  # prevents NameError for typehints
+from datetime import datetime
+
 from fastapi import FastAPI
+
+from chatbot import schemas
 
 app = FastAPI()
 
@@ -18,8 +23,8 @@ def new_message():
 
 
 @app.get("/surveys/{survey_id}/questions/{question_id}")
-def question(survey_id: int, question_id: int):
-    """The endpoint that is called by the TwiML redirect function after a new
+def get_question(survey_id: int, question_id: int):
+    """The endpoint called by the TwiML redirect function after a new
     survey is initiated or an answer to a survey question is processed
     """
     return {"survey": survey_id, "question": question_id}
@@ -29,8 +34,62 @@ def question(survey_id: int, question_id: int):
     "/surveys/{survey_id}/questions/{question_id}/answers",
     status_code=201,
 )
-def answer(survey_id: int, question_id: int):
+def create_answer(survey_id: int, question_id: int):
     """The endpoint that is called by the TwiML redirect function after a
     response to a survey question is received
     """
     return {"survey": survey_id, "answer": question_id}
+
+
+@app.post(
+    "/surveys",
+    response_model=schemas.SurveyQuestionsOut,
+    status_code=201,
+)
+def create_survey(survey_data: schemas.SurveyQuestionsIn):
+    """Create a new survey"""
+    print(survey_data.name)
+    result = {
+        "id": 1,
+        "name": "Welcome survey",
+        "created_date": datetime(2021, 11, 2),
+        "questions": [
+            {
+                "id": 1,
+                "text": "Question 1",
+                "created_date": datetime(2021, 11, 2),
+            },
+            {
+                "id": 2,
+                "text": "Question 2",
+                "created_date": datetime(2021, 11, 2),
+            },
+        ],
+    }
+    response_data = schemas.SurveyQuestionsOut(**result)
+    return response_data
+
+
+@app.put("/surveys/{survey_id}", response_model=schemas.SurveyQuestionsOut)
+def update_survey(survey_data: schemas.SurveyQuestionsIn):
+    """Update the questions on an existing survey"""
+    print(survey_data.name)
+    result = {
+        "id": 1,
+        "name": "Welcome survey",
+        "created_date": datetime(2021, 11, 2),
+        "questions": [
+            {
+                "id": 1,
+                "text": "Question 1",
+                "created_date": datetime(2021, 11, 2),
+            },
+            {
+                "id": 2,
+                "text": "Question 2",
+                "created_date": datetime(2021, 11, 2),
+            },
+        ],
+    }
+    response_data = schemas.SurveyQuestionsOut(**result)
+    return response_data
